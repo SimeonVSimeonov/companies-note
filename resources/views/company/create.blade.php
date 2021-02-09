0abfc3024735fc38ca97918816e94f432baf3294
simeonoff <simeonsimeonoff@icloud.com>
2021-02-09 16:17:50 +0200

    add group in modal form

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -22,27 +22,59 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Company Group') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="group" type="radio" class="form-control @error('name') is-invalid @enderror"
-                                   name="group_id" value="{{null}}" required autocomplete="name" autofocus>
-                            <label for="none">None</label><br>
-                            @foreach($groups as$group)
-                            <input id="group" type="radio" class="form-control @error('name') is-invalid @enderror"
-                                   name="group_id" value="{{$group->id}}" required autocomplete="name" autofocus>
-                                <label for="{{$group->name}}">{{$group->name}}</label><br>
-                            @endforeach
-                            @error('name')
-                            <span class="invalid-feedback" role="alert">
+                    <div id="modal" class="modal fade" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Company Group Options</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="row-sm-2 text-center">
+                                        <label for="none"><input id="group" type="radio"
+                                                                 class="form-control @error('name') is-invalid @enderror"
+                                                                 name="group_id" value="{{null}}" required
+                                                                 autocomplete="name"
+                                                                 autofocus>None</label>
+                                        @foreach($groups as$group)
+                                            <label for="{{$group->name}}"><input id="group" type="radio"
+                                                                                 class="form-control @error('name') is-invalid @enderror"
+                                                                                 name="group_id" value="{{$group->id}}"
+                                                                                 required
+                                                                                 autocomplete="name"
+                                                                                 autofocus>{{$group->name}}</label>
+                                        @endforeach
+                                        @error('name')
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                            @enderror
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Save changes</button>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
+                    <div class="form-group row">
+                        <label for="name"
+                               class="col-md-4 col-form-label text-md-right">{{ __('Company Group Options') }}</label>
+                        <div class="col-md-6">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal">
+                                Show Groups
+                            </button>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="name"
+                               class="col-md-4 col-form-label text-md-right">{{ __('Company Group Selected') }}</label>
+                        <div id="selectedGroup" class="col-md-6">
 
+                        </div>
+                    </div>
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
                             <button type="submit" class="btn btn-primary">
@@ -54,5 +86,15 @@
             </div>
         </div>
     </div>
+    <script  type="application/javascript">
+        window.onload = function() {
+            if (window.jQuery) {
+                $('#modal').on('hidden.bs.modal', function (e) {
+                    var l = $('input[name="group_id"]:checked').closest('label').text();
+                    $('#selectedGroup').append(`<strong>${l}</strong>`)
+                })
+            }
+        }
+    </script>
 @endsection
 
